8201cd1c5c5fafa4d26f9c3c2f696bc836bd9e45
Ivan Berezutskiy <santiago_bravo@mail.ru>
2024-03-10 20:23:44 +0300

    project finished

--- src/main/java/org/example/GuessNumberBot.java
+++ src/main/java/org/example/GuessNumberBot.java
@@ -17,23 +17,56 @@ public class GuessNumberBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage()) {
+        if (update.hasMessage() == true) {
+            Message messageFromUser = update.getMessage();
+
+            if (messageFromUser.hasText() == true) {
+                String textFromMessage = messageFromUser.getText();
+
+                // log2 (диапазон)
+
+                String chatId = messageFromUser.getChatId().toString();
+                // -2^64;2^64-1
+
+                if (textFromMessage.equals("/start")) {
+                    targetNumber = random.nextInt(101);
+                    sendMessage(chatId, "Привет! Я загадал число от 0 до 100! Попробуй отгадать :)");
+                } else {
+                    int guessNumber = Integer.parseInt(textFromMessage);
+
+                    if (guessNumber == targetNumber) {
+                        sendMessage(chatId, "Ура! Ты отгадал число!");
+                    } else if(guessNumber < targetNumber) {
+                        sendMessage(chatId, "Загаданное число больше введеного :((");
+                    } else {
+                        sendMessage(chatId, "Загаданное число меньше введеного :((");
+                    }
+                }
+            }
+        }
+    }
+
+    void sendMessage(String chatId, String textFromMessage) {
+        SendMessage sendMessage = new SendMessage(chatId, textFromMessage);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
 
         }
     }
 
 
-
     @Override
     public String getBotUsername() {
         // Здесь нужно указать имя бота
-        return "YourBotUsername";
+        return "openlessondemotenmarchbot";
     }
 
     @Override
     public String getBotToken() {
         // Здесь нужно указать токен, полученный от BotFather
-        return "YourBotToken";
+        return "7065100018:AAHBQZYGOqTOx80-FmVK4pUzHayd4icCvtM";
     }
 
     public static void main(String[] args) throws TelegramApiException {
